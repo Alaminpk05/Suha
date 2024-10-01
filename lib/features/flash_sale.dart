5708b5b7347b4a723faf1604f0c520c30b593989
Alaminpk05 <mdalaminpk05@gmail.com>
2024-10-01 18:31:19 +0600

    working on flash fetch

--- lib/features/flash_sale.dart
+++ lib/features/flash_sale.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:shuhaui/features/home/Widgets/singlewidgets/cycloneofferCard.dart';
+import 'package:shuhaui/features/home/data/model/cycloneoffer.dart';
+import 'package:shuhaui/features/home/data/repository/load_product_data.dart';
 import 'package:shuhaui/utils/constant.dart';
+import 'package:shuhaui/utils/dependency_injection/dependency_setup.dart';
 import 'package:shuhaui/utils/global_widgets/circuler_menu.dart';
 import 'package:shuhaui/utils/respnsive_helper.dart';
 
-class FlashSale extends StatelessWidget {
+class FlashSale extends StatefulWidget {
   const FlashSale({super.key});
+
+  @override
+  State<FlashSale> createState() => _FlashSaleState();
+}
+
+class _FlashSaleState extends State<FlashSale> {
+ late Future<List<CycloneOfferModel>> datalist;
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    datalist=getIt<ProductService>().fetchFlashProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,26 +36,34 @@ class FlashSale extends StatelessWidget {
           filterwidget(
             mobile: mobile,
           )),
-      body: Padding(
-          padding: EdgeInsets.symmetric(vertical: 1.5.h, horizontal: 2.w),
-          child: GridView.builder(
-              itemCount: 6,
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1, childAspectRatio: 1.945),
-              itemBuilder: (context, index) {
-                return Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(3, (index) {
-                      var item = cycloneOfferItems[index];
-                      return CycloneOfferWidget(
-                        value: item['value'],
-                        asset: item['asset'],
-                        title: item['title'],
-                      );
-                    }));
-              })),
+      body: FutureBuilder(
+        future: datalist,
+        builder: (context,snapshot){
+          final itemlist=snapshot.data!;
+          return Padding(
+              padding: EdgeInsets.symmetric(vertical: 1.5.h, horizontal: 2.w),
+              child: GridView.builder(
+                  itemCount: itemlist.length,
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1, childAspectRatio: 1.945),
+                  itemBuilder: (context, index) {
+                    final item=itemlist[index];
+                    return Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(3, (index) {
+                          var item = cycloneOfferItems[index];
+                          return CycloneOfferWidget(
+                            value: item['value'],
+                            asset: item['asset'],
+                            title: item['title'],
+                          );
+                        }));
+                  }));
+        },
+
+      ),
     );
   }
 }
